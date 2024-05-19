f0735acae6d03c6c61c2896c02710c69450a6332
Rebekah <macrosystem@proton.me>
2024-05-19 23:53:44 +0200

    Update run-me.py

--- run-me.py
+++ run-me.py
@@ -3,7 +3,7 @@ import runpy
 scripts_to_run = [  'scripts/my-script.py',
           
                     'shellrc-append/append-to-bashrc.py',
-                    'scripts/utility/source-bashrc.sh',
+                    'scripts/utility/source-bashrc.py',
 ]
 
 def run(path):
