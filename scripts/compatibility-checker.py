8137f696387f9cac049db5162a241ce8ffda6bfd
Rebekah <macrosystem@proton.me>
2024-05-24 16:31:17 +0200

    Update compatibility-checker.py

--- scripts/compatibility-checker.py
+++ scripts/compatibility-checker.py
@@ -7,7 +7,9 @@ def is_arch():
     return True #FIX ME
   
 def is_plasma():
-    return True #FIX ME
+    bool_installed = True
+    bool_running = True
+    return bool_installed and bool_running #FIX ME
 
 def is_compatible():
   return (is_linux() and is_arch() and is_plasma()
