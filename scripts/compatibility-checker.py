66bb21a3d4612c207b0d3646bca4a4f9b6266287
Rebekah <macrosystem@proton.me>
2024-05-26 13:59:29 +0200

    Update compatibility-checker.py

--- scripts/compatibility-checker.py
+++ scripts/compatibility-checker.py
@@ -1,4 +1,5 @@
 import platform
+import subprocess
 
 def is_linux():
     return platform.system() == 'Linux'
@@ -7,8 +8,12 @@ def is_arch():
     return True #FIX ME
   
 def is_plasma(): 
-    bool_installed = True #FIX ME
+    
     bool_running = True #FIX ME
+
+    bool_plasma6_installed = "plasmashell 6" in (subprocess.run("plasmashell --version", shell=True))
+    bool_installed = bool_plasma5_installed
+    
     return bool_installed and bool_running
 
 def is_compatible():
